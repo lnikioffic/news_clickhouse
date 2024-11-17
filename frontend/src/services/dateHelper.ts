1981657d734cce6b137161ae351884dc8e87db2a
Danil <danil0316@mail.ru>
2024-11-17 16:27:30 +0500

    fix dateHelper

--- frontend/src/services/dateHelper.ts
+++ frontend/src/services/dateHelper.ts
@@ -1,6 +1,6 @@
 export function getDateString(dateStr: string): string {
     const date = new Date(dateStr)
-    const day = date?.getDay() ?? '00'
+    const day = date?.getDate() ?? '00'
     const month = date?.getMonth() ?? '00'
     const year = date?.getFullYear() ?? '0000'
 
